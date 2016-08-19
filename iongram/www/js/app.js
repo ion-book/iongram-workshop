2041cb475e9165779f241673d1f89cc16feb54bd
Carlos rojas <ing.carlosandresrojas@gmail.com>
2016-08-19 20:13:03 -0300

    router

--- iongram/www/js/app.js
+++ iongram/www/js/app.js
@@ -23,63 +23,64 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state('login',{
+    url: '/login',
+    templateUrl: 'templates/login.html'
+  }
+  )
+  .state('tab',{
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
+  }
+  )
+  .state('tab.home',{
+    url: '/home',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-home': {
+        templateUrl: 'templates/tab-home.html'
+      }
+    }
+  }
+  )
+  .state('tab.search',{
+    url: '/search',
+    views:{
+      'tab-search': {
+        templateUrl: 'templates/tab-search.html'
       }
     }
   })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.activity',{
+    url: '/profile',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-profile': {
+        templateUrl: 'templates/tab-profile.html'
       }
     }
+  }
+  )
+  .state('map',{
+    url: '/map',
+    templateUrl: 'templates/map.html'
+  })
+  .state('camera',{
+    url: '/camera',
+    templateUrl: 'templates/camera.html'
+  })
+  .state('filters',{
+    url: '/filters',
+    templateUrl: 'templates/filters.html'
+  })
+  .state('comments',{
+    url: '/comments/:id',
+    templateUrl: 'templates/comments.html'
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
+  $urlRouterProvider.otherwise('/login');
+  $ionicConfigProvider.tabs.position('bottom');
+  
 });
